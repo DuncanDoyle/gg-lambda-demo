6c6962bc4b1e03431cef7ce9966214b4cabc3e35
Duncan Doyle <Duncan.Doyle@gmail.com>
2024-10-08 19:51:47 +0200

    Fixed call to lambda and prefix replacement in VS.

--- curl-request-k8s.sh
+++ curl-request-k8s.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-curl -v -X POST http://lambda.example.com/ -d '{"name": "Bill",  "greeting": "hello"}'
-
-# curl -v -X POST -H "Host: lambda.example.com" http://127.0.0.1:8000/ -d '{"name": "Bill",  "greeting": "hello"}'
+curl -v -X POST http://lambda.example.com:81/test/ -d '{"name": "Bill",  "greeting": "hello"}'
